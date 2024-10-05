05dfc713c890664d0bb5a679457a766cbd09d49c
joaomigueld3 <joaomigueld3@gmail.com>
2024-10-05 19:41:51 -0300

    Setup do projeto

---
+++ src/index.js
@@ -0,0 +1,27 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+// app.use(bodyParser.json());
+
+// Conectar ao MongoDB
+/* mongoose.connect('mongodb://localhost:27017/usercrud', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+}).then(() => console.log('Conectado ao MongoDB'))
+  .catch(err => console.error('Erro ao conectar ao MongoDB', err)); */
+
+// Rotas
+// app.use('/api', userRoutes);
+
+// Iniciar o servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
